6d32a636a3de02cbb275d4b21bade612ac5a9efc
F-Fischer <francisco.fischer92@gmail.com>
2015-07-19 21:47:48 -0300

    Internacionalizacion bien (FRAN MECHANICS)

--- Usuarios-Login/src/com/usuarioslogin/filtros/FiltroAutenticacion.java
+++ Usuarios-Login/src/com/usuarioslogin/filtros/FiltroAutenticacion.java
@@ -21,7 +21,7 @@ public class FiltroAutenticacion implements Filter {
 
 	
 	private String[] 
-			rnp = "/login,/login.html,/bower_components/jquery/dist/jquery.min.js,/bower_components/bootstrap/dist/js/bootstrap.min.js,/bower_components/metisMenu/dist/metisMenu.min.js,dist/js/sb-admin-2.js,/bower_components/bootstrap/dist/css/bootstrap.min.css,/bower_components/metisMenu/dist/metisMenu.min.css,/dist/css/sb-admin-2.css,/bower_components/font-awesome/css/font-awesome.min.css".
+			rnp = "/gettranslation,/login,/login.html,/bower_components/jquery/dist/jquery.min.js,/bower_components/bootstrap/dist/js/bootstrap.min.js,/bower_components/metisMenu/dist/metisMenu.min.js,dist/js/sb-admin-2.js,/bower_components/bootstrap/dist/css/bootstrap.min.css,/bower_components/metisMenu/dist/metisMenu.min.css,/dist/css/sb-admin-2.css,/bower_components/font-awesome/css/font-awesome.min.css".
 			toLowerCase().split(",");
 	
     public FiltroAutenticacion() {
